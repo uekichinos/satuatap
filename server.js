61e7b232b0d469084718e78b21cecd13c8f2a79d
Ueki Chinos <megatzulkhairi@gmail.com>
2026-06-02 21:02:36 +0800

    Update page title to "Satu Atap"

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ function renderPage() {
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>satuatap.app</title>
+  <title>Satu Atap</title>
   <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@600&display=swap" rel="stylesheet" />
